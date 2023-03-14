d18ee7476c10d0ae4d073bcd1ea06b53f07ea695
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2023-03-14 20:58:07 -0300

    Agregar archivos de proyecto.

---
+++ Rodri1/PrimerForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rodri1
+{
+    public partial class PrimerForm : Form
+    {
+        public PrimerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text =System.Convert.ToString( 
+                    System.Convert.ToInt32(txtA.Text )
+                    + System.Convert.ToInt32(txtB.Text)
+                );
+        }
+    }
+}
---
+++ Rodri1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rodri1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            PrimerForm pepe = new PrimerForm(); 
+            Application.Run(pepe);
+        }
+    }
+}
